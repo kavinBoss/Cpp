c274039ef7439e43e664e9af4bd1bc4edb996b61
Kavin <kavin@localhost.localdomain>
2022-10-18 22:11:07 +0530

    Finding Average

---
+++ Cylinder CSA.cpp
@@ -0,0 +1,12 @@
+//To find CSA of cylinder CSA=2 pi r*h
+#include<iostream>
+using namespace std;
+int main(){
+    float Radius,Height,CSA;
+    cout<<"enter radius and height of cylinder:";
+    cin>>Radius>>Height;
+    CSA=2*3.14*Radius*Height;
+    cout<<"CSA of cylider is:"<<CSA;
+
+}
+
---
+++ Dynamic Initialization.cpp
@@ -0,0 +1,13 @@
+//Average
+#include<iostream>
+using namespace std;
+ int main(){
+    float num1,num2,num3,sum,avg;
+    cout<<"Enter number 1 and 2 and 3:";
+    cin>>num1>>num2>>num3;
+    sum=num1+num2+num3;
+    avg=sum/3;
+    cout<<"Your Average is:"<<avg;
+
+ }
+
