c88a50347ba7d76c6886a46229304b9d67cecf11
Kavin <kavin@localhost.localdomain>
2022-10-18 18:43:11 +0530

    Initialize

---
+++ Area_of_circle.cpp
@@ -0,0 +1,10 @@
+//Find Area Of Circle
+#include<iostream>
+using namespace std;
+int main(){
+    float radius,area;
+    cout<<"Enter Radius Of Circle:"<<endl;
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"Area of circle is:"<<endl<<area;
+}
---
+++ Marks_of_five_subjects.cpp
@@ -0,0 +1,10 @@
+//To Find Total Marks Of 5 Subjects
+#include<iostream>
+using namespace std;
+int main(){
+    int m1,m2,m3,m4,m5,total;
+    cout<<"Enter marks of five subjects\n";
+    cin>>m1>>m2>>m3>>m4>>m5;
+    total=m1+m2+m3+m4+m5;
+    cout<<"\nYour total mark is:"<<total;
+}
